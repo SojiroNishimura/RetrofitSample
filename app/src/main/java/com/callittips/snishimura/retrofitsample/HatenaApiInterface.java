fbf2fdece7c507ffd3a87b94d2fce2ab7fee24ed
Sojiro Nishimura <chartreux.nyao@gmail.com>
2016-04-17 15:11:55 +0900

    Add api to get hotentries rss

--- app/src/main/java/com/callittips/snishimura/retrofitsample/HatenaApiInterface.java
+++ app/src/main/java/com/callittips/snishimura/retrofitsample/HatenaApiInterface.java
@@ -1,9 +1,11 @@
 package com.callittips.snishimura.retrofitsample;
 
 import com.callittips.snishimura.retrofitsample.data.BookmarkContainer;
+import com.callittips.snishimura.retrofitsample.data.rss.Rdf;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -12,7 +14,11 @@ import retrofit2.http.Query;
 public interface HatenaApiInterface {
     String END_POINT = "http://b.hatena.ne.jp";
     String TARGET_URL = "http://b.hatena.ne.jp/ctop/it";
+    String CATEGORY_IT = "it.rss";
 
     @GET("/entry/jsonlite/")
     Call<BookmarkContainer> getBookmarksWithUrl(@Query("url") String target);
+
+    @GET("/hotentry/{category}")
+    Call<Rdf> getHotentriesWithCategory(@Path("category") String category);
 }
